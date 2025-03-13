03fef247c04a91e8163c2e3392f0f3d1ebaf2077
yuanmingqi <fredrichyuan19990827@gmail.com>
2025-03-13 10:55:14 -0400

    update

--- gradio/test.py
+++
@@ -1,7 +0,0 @@
-# svg to png
-import cairosvg
-
-cairosvg.svg2png(url="logo_polyu.svg", write_to="logo_polyu.png")
-cairosvg.svg2png(url="logo_sjtu.svg", write_to="logo_sjtu.png")
-cairosvg.svg2png(url="logo_purdue.svg", write_to="logo_purdue.png")
-cairosvg.svg2png(url="logo_ustc.svg", write_to="logo_ustc.png")
